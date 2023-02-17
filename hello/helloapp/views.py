14fed79843879cca50635999990f5ff610267ac4
sombhu <hightechxyz@gmail.com>
2023-02-17 17:24:58 +0530

    no change

--- hello/helloapp/views.py
+++ hello/helloapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hii  friends i am sombhu sombhu ")
+    return HttpResponse("hii  friends i am sombhu and this is my first page in django ")
